a74f6962b9180e604c678bc8cdbcf43933193723
Lee Jones <lee@lees-mbp.lan>
2021-03-18 00:34:13 +0000

    shows user input

---
+++ js/app.js
@@ -0,0 +1,33 @@
+
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+todoButton.addEventListener('click', addToDo)
+
+function addToDo(event) {
+    // Prevent form from submitting
+    event.preventDefault();
+    // To do div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // Create a li element
+    const newToDo = document.createElement('li');
+    newToDo.innerText = 'test';
+    newToDo.classList.add('todo-item');
+
+    todoDiv.appendChild(newToDo); // puts newToDo in the todoDiv => div with an li node 
+    //Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-clipboard-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    // trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(completedButton); 
+    // append to list
+    todoList.appendChild(todoDiv);
+
+}
