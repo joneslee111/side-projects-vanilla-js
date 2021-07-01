168418f48704d3c7fa1e5901960f8dec23dbd0df
Lee Jones <leejones111@hotmail.co.uk>
2021-07-01 17:17:41 +0100

    added an addItem function

--- grocery-list/app.js
+++ grocery-list/app.js
@@ -1,5 +1,4 @@
 // ****** SELECT ITEMS **********
-
 const alert = document.querySelector('.alert');
 const form = document.querySelector('.grocery-form');
 const grocery = document.getElementById('grocery');
@@ -9,15 +8,28 @@ const list = document.querySelector('.grocery-list');
 const clearBtn = document.querySelector('.clear-btn');
 
 // edit option
-
 let editElement;
 let editFlag = false;
 let editID = "";
 
 // ****** EVENT LISTENERS **********
+//submit form
+form.addEventListener('submit', addItem);
 
 // ****** FUNCTIONS **********
-
+function addItem(e) {
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+    if (value && !editFlag) {
+        console.log('add item to the list');
+    }
+    else if (value && editFlag) {
+        console.log('editing');
+    } else {
+        console.log('empty value');
+    }
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
